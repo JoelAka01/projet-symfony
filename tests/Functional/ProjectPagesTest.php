31707e85f915d57c420887121469523ca0f560da
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-06-09 22:30:12 +0200

    functional tests for project pages

---
+++ tests/Functional/ProjectPagesTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class ProjectPagesTest extends WebTestCase
+{
+    public function testProjectListRequiresAuthentication(): void
+    {
+        $client = self::createClient();
+
+        $client->request('GET', '/projects');
+
+        self::assertResponseRedirects('/login');
+    }
+
+}
