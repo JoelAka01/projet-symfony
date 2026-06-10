a21a799bba03eb6c8c760d318f11cf4408740a32
madoxSio <mahamadou.gory.sio@gmail.com>
2026-06-10 11:10:35 +0200

    ci: ajout pipeline GitHub Actions, linter et tests de mutation
    - GitHub Actions : 4 jobs (lint, static-analysis, tests, mutation)
    - PHP CS Fixer : vérification du style @Symfony + @PER-CS2.0
    - Infection : tests de mutation avec seuils MSI 50%/70%
    - composer.json : ajout friendsofphp/php-cs-fixer et infection/infection
    - Makefile + README : nouvelles commandes lint, lint-fix, mutation, ci-local

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__.'/src')
+    ->in(__DIR__.'/tests');
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setRules([
+        '@Symfony' => true,
+        '@PER-CS2.0' => true,
+        'declare_strict_types' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arrays', 'arguments', 'parameters']],
+    ])
+    ->setFinder($finder);
