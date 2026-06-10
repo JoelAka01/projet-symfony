a0220997abc98bb64308cbd5f777870cea8455dd
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-06-10 12:41:27 +0200

    claude json parsing unit test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,12 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+$envFile = dirname(__DIR__).'/.env';
+if (!is_file($envFile)) {
+    $envFile = dirname(__DIR__).'/.env.example';
+}
+
+(new Dotenv())->bootEnv($envFile);
 
 if (filter_var($_SERVER['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOL)) {
     umask(0000);
