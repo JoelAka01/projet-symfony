ab977479b05c5e8852be6bc2ca35f99e8f62b31c
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-06-12 23:00:21 +0200

    upgrade btn for users already subscripbed

---
+++ src/Twig/SubscriptionExtension.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use App\Entity\User;
+use App\Repository\SubscriptionRepository;
+use App\Repository\AnalysisQuotaUsageRepository;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+final class SubscriptionExtension extends AbstractExtension
+{
+    public function __construct(
+        private readonly SubscriptionRepository $subscriptionRepository,
+        private readonly AnalysisQuotaUsageRepository $quotaUsageRepository,
+    ) {}
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('has_active_subscription', [$this, 'hasActiveSubscription']),
+            new TwigFunction('has_achieved_weekly_limit', [$this, 'hasAchievedWeeklyLimit']),
+        ];
+    }
+
+    public function hasActiveSubscription(?User $user): bool
+    {
+        if (null === $user) {
+            return false;
+        }
+
+        return null !== $this->subscriptionRepository->findActiveForUser($user);
+    }
+
+    public function hasAchievedWeeklyLimit(?User $user): bool
+    {
+        if (null === $user) {
+            return false;
+        }
+
+        $subscription = $this->subscriptionRepository->findActiveForUser($user);
+        if (null === $subscription) {
+            return false;
+        }
+
+        $weekStartedAt = new \DateTimeImmutable('monday this week 00:00:00');
+        if ($weekStartedAt < $subscription->getStartsAt()) {
+            $weekStartedAt = $subscription->getStartsAt();
+        }
+
+        $weeklyUsed = $this->quotaUsageRepository->countForUserSince($user, $weekStartedAt);
+        return $weeklyUsed >= $subscription->getWeeklyAnalysisLimit();
+    }
+}
