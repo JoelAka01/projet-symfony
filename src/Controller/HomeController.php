9079dafb01c545f44d1dd67d87a901e822505790
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-06-12 23:00:21 +0200

    upgrade btn for users already subscripbed

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\Billing\PlanCatalog;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,12 +12,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home', methods: ['GET'])]
-    public function index(): Response
+    public function index(PlanCatalog $planCatalog): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('app_project_index');
         }
 
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'plans' => $planCatalog->all(),
+        ]);
     }
 }
