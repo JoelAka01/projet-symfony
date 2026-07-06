c9c4479a4490e11d0e79e7fcda84e886fd99f413
Dilan EESHVARAN <ieeshvarand@gmail.com>
2026-07-06 20:38:54 +0200

    project access repos

---
+++ migrations/Version20260706130000.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260706130000 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add guest access levels to project guests and invitations';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("ALTER TABLE project_guests ADD access VARCHAR(20) DEFAULT 'content' NOT NULL");
+        $this->addSql("ALTER TABLE project_invitations ADD access VARCHAR(20) DEFAULT 'content' NOT NULL");
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE project_guests DROP access');
+        $this->addSql('ALTER TABLE project_invitations DROP access');
+    }
+}
